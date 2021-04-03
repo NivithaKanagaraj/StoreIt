a1a310123ef36a4955ff69a498c4cb886bef6efb
Nivitha <aimsnivitha@gmail.com>
2021-04-04 01:15:25 +0530

    changed axios code

--- task_react/src/Components/ShareFile.js
+++ task_react/src/Components/ShareFile.js
@@ -80,7 +80,7 @@ class ShareFile extends React.Component{
                             filename: filename,
                         });
 
-                    axios.post(`http://localhost:8000/upload`,formData,options).then(res=>{
+                    axios.post(`https://store--it.herokuapp.com/upload`,formData,options).then(res=>{
                         //data.delete('file')
                         console.log(res);
                     this.setState({
